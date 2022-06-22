02f2235dd895ea631ad4317caeac97f470d86b91
c-noblet <c.noblet.dev@gmail.com>
2022-06-22 16:48:47 +0200

    add Player

---
+++ components/Player.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button ref="soundBtn" class="sound-btn" @click="handlePlayer" />
+    <audio ref="audio">
+      <source src="/jazzy.mp3" type="audio/mpeg">
+    </audio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PlayerComponent',
+  data () {
+    return {
+      playing: false
+    }
+  },
+  methods: {
+    handlePlayer () {
+      this.playing = !this.playing
+      if (this.playing) {
+        console.log('play')
+        this.$refs.audio.play()
+        this.$refs.soundBtn.classList.add('--active')
+        this.$refs.audio.volume = 1
+      } else {
+        console.log('pause')
+        this.$refs.audio.pause()
+        this.$refs.soundBtn.classList.remove('--active')
+      }
+    }
+  }
+}
+</script>
