37206456799901f453e75e64755010fdb8c66602
vgimonnet <valentin.gimonnet18@gmail.com>
2022-06-23 12:13:15 +0200

    Animation modal

--- components/ModalDetail.vue
+++ components/ModalDetail.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="modal" id="modalDetail" v-if="showModal" @click="onClickOut($event)">
-    <div class="modal-dialog" :style="position">
+  <div class="modal" id="modalDetail"  @click="onClickOut($event)">
+      <div class="modal-dialog" :style="position">
 
-      <button aria-label="Fermer la fenêtre de détail" class="btn__close" @click="closeModal">
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <line x1="17.4156" y1="6.49174" x2="6.20859" y2="17.6987" stroke="#191919"/>
-          <line x1="17.4555" y1="17.6985" x2="6.24855" y2="6.49155" stroke="#191919"/>
-        </svg>
-      </button>
+        <button aria-label="Fermer la fenêtre de détail" class="btn__close" @click="closeModal">
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <line x1="17.4156" y1="6.49174" x2="6.20859" y2="17.6987" stroke="#191919"/>
+            <line x1="17.4555" y1="17.6985" x2="6.24855" y2="6.49155" stroke="#191919"/>
+          </svg>
+        </button>
 
-      <div class="modal-header">
-        <h2>
-          {{ data.date }}
-        </h2>
-        <h3 v-html="data.title"></h3>
+        <div class="modal-header">
+          <h2>
+            {{ data.date }}
+          </h2>
+          <h3 v-html="data.title"></h3>
+        </div>
+
+        <section class="modal-content">
+          <p v-html="data.content"></p>
+        </section>
+
+        <div class="modal-footer">
+          <!-- <a href="#" @click="closeModal()" class="btn-line">
+            Acheter un billet -->
+            <!-- <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M0.97998 1.29272H11.4545V11.7673" stroke="#191919"/>
+              <line x1="0.782189" y1="11.9907" x2="11.1012" y2="1.67172" stroke="#191919"/>
+            </svg> -->
+          <!-- </a> -->
+          <button class="btn-line ml-auto" @click="closeModal">Acheter un billet</button>
+        </div>
       </div>
-
-      <section class="modal-content">
-        <p v-html="data.content"></p>
-      </section>
-
-      <div class="modal-footer">
-        <!-- <a href="#" @click="closeModal()" class="btn-line">
-          Acheter un billet -->
-          <!-- <svg width="12" height="13" viewBox="0 0 12 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M0.97998 1.29272H11.4545V11.7673" stroke="#191919"/>
-            <line x1="0.782189" y1="11.9907" x2="11.1012" y2="1.67172" stroke="#191919"/>
-          </svg> -->
-        <!-- </a> -->
-        <button class="btn-line ml-auto" @click="closeModal">Acheter un billet</button>
-      </div>
-    </div>
   </div>
 </template>
 
@@ -51,7 +51,8 @@ export default {
   props: ['showModal', 'data', 'index'],
   data () {
     return {
-      position: positions[this.index]
+      position: positions[this.index],
+      show: ''
     }
   },
   emits: ['close-modal'],
@@ -65,11 +66,23 @@ export default {
         console.log('ok')
         this.closeModal()
       }
+    },
+    mounted () {
+      this.show = 'show'
     }
   },
   watch: {
     index (val) {
       this.position = positions[val]
+    },
+    showModal (val) {
+      if (val) {
+        // this.show = 'show'
+        document.querySelector('.modal-dialog').classList.add('show')
+      } else {
+        document.querySelector('.modal-dialog').classList.remove('show')
+        // this.show = ''
+      }
     }
   }
 }
--- components/Museum.vue
+++ components/Museum.vue
@@ -346,11 +346,13 @@ export default {
   methods: {
     closeModal () {
       this.showModal = false
+      this.toggleBtnDetailVisibility(true)
     },
     showDetail (index) {
       this.indexOeuvre = index
       this.oeuvre = this.oeuvres[index]
       this.showModal = !this.showModal
+      this.toggleBtnDetailVisibility(false)
     },
     zoom (event, idAnchor = 0) {
       this.zoomActif = !this.zoomActif
@@ -400,6 +402,15 @@ export default {
     scrollToStart () {
       document.querySelector('#two').scrollIntoView({
         behavior: 'smooth'
+      })
+    },
+    toggleBtnDetailVisibility (show) {
+      document.querySelectorAll('.btn__detail').forEach((btn) => {
+        if (show) {
+          btn.classList.remove('__hidden')
+        } else {
+          btn.classList.add('__hidden')
+        }
       })
     }
   }
